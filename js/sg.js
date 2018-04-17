1393a8c716c01a0d6c587eb4dde2f2cb056c23a0
Brent Rogers <contact@brentjrogers.com>
2018-04-16 21:06:08 -0400

    Create sg.js

---
+++ js/sg.js
@@ -0,0 +1,21 @@
+document.body.onload = (function() {
+
+var m = document.createElement('meta');
+m.setAttribute('charset', 'utf-8');
+m.setAttribute('name', 'viewport');
+m.setAttribute('content', 'width=device-width,initial-scale=1,user-scalable=0');
+
+var l = document.createElement('link');
+l.setAttribute('rel', 'stylesheet');
+l.setAttribute('href', 'http://shapegraph.com/cdn/css/sg.css');
+l.setAttribute('type', 'text/css');
+
+var s = document.createElement('script');
+s.setAttribute('type', 'text/javascript');
+s.setAttribute('src', 'https://code.jquery.com/jquery-3.3.1.min.js');
+
+document.getElementsByTagName('head')[0].appendChild(m);
+document.getElementsByTagName('head')[0].appendChild(l);
+document.getElementsByTagName('head')[0].appendChild(s);
+
+}());
